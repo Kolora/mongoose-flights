9f092dd1356189175e0a6d383feb5b645367e3a3
Divya <divya.kolora@gmail.com>
2023-08-21 09:27:20 -0600

    database connected

---
+++ routes/flightsRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const router = express.Router();
+
+const flightsController = require("../controllers/flightsController.js");
+
+// "index" route GET
+router.get("/", flightsController.index);
+
+// "new" route GET
+router.get("/new", flightsController.new);
+
+// "update" route PUT
+router.put("/:id", flightsController.update);
+
+// "create" route POST
+router.post("/", flightsController.create);
+
+// "Show" route GET
+router.get("/:id", flightsController.show);
+
+module.exports = router;
